b2c5a8563796cefc4b413cec52d26f6dddcad3dd
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2022-12-27 18:04:23 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload')
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE;
+
+// Database Connection
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connection Successful');
+}).catch((err) => {
+    console.log(err);
+});
+
+// Increasing the http request size
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+
+// Router
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+}
+
+app.listen(PORT,() => console.log(`Server started at ${PORT}`));
